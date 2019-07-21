6ba6418c2a6c75a3464ccd6b034e93d4ce0d920a
Guoxi <liuguoxi@chd.edu.cn>
2019-07-21 19:19:35 -0400

    Problem 136, 141, 155 (July 17th)

---
+++ 136.single-number.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=136 lang=cpp
+ *
+ * [136] Single Number
+ */
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        int res = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            res ^= nums[i];
+        }
+        return res;
+    }
+};
+
