b4d80711231af224d459d44b1da3fda49dc7f92d
Guoxi <liuguoxi@chd.edu.cn>
2021-11-22 00:00:12 -0500

    2021.11.20 challenge

---
+++ 540.single-element-in-a-sorted-array.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=540 lang=cpp
+ *
+ * [540] Single Element in a Sorted Array
+ *
+ * https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+ *
+ * algorithms
+ * Medium (58.08%)
+ * Likes:    4114
+ * Dislikes: 101
+ * Total Accepted:    249.7K
+ * Total Submissions: 425.6K
+ * Testcase Example:  '[1,1,2,3,3,4,4,8,8]'
+ *
+ * You are given a sorted array consisting of only integers where every element
+ * appears exactly twice, except for one element which appears exactly once.
+ * 
+ * Return the single element that appears only once.
+ * 
+ * Your solution must run in O(log n) time and O(1) space.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [1,1,2,3,3,4,4,8,8]
+ * Output: 2
+ * Example 2:
+ * Input: nums = [3,3,7,7,10,11,11]
+ * Output: 10
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * 0 <= nums[i] <= 10^5
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
